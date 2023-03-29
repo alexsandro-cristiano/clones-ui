8eae3fed49fe00f32c8ea379882a55f00cb81f2b
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2023-03-28 21:02:42 -0300

    feat:adiciona css global

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -1,5 +1,10 @@
-export function App(){
-  return(
-    <h2>inicio da aplicação</h2>
-  )
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h2>inicio da aplicação</h2>
+    </>
+  );
 }
